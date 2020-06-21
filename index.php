7e5a265d183bfa2bdba3d4ef514b8ab20a18e2d8
triplem9504 <63857433+triplem9504@users.noreply.github.com>
2020-06-21 15:07:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello Guys, I am Muhamad Mustaqim, the Cloud Computer expert!." . PHP_VERSION . "\n";
+print "Hello Guys, I am Mustaqim, the Cloud Computer expert!. Please go to my GitHub account "https://github.com/triplem9504" . PHP_VERSION . "\n";
 ?>
